62e7df92c52f993e000fcbdb7dd1d36c4f22e191
Sean McGee <d00121058@dkit.ie>
2018-03-01 15:47:15 +0000

    Angular App set up

---
+++ app/app-state-manager.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function () {
+
+    angular.module('accountApp').config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/dashboard");
+
+        $stateProvider.state("dashboard", {
+            url: "/dashboard",
+            templateUrl: "app/feature/dashboard/dashboard.html"
+        })
+    });
+}());
