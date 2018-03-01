fa9eaa3eeb935aad6d1f177313a3beb7011e7557
Sean McGee <d00121058@dkit.ie>
2018-03-01 17:03:53 +0000

    Simple API call made and outputed

--- app/feature/bbva/bbva-controller.js
+++ app/feature/bbva/bbva-controller.js
@@ -1,13 +1,19 @@
 (function() {
 
-    var BBVAController =  function() {
+    var BBVAController =  function($http) {
 
     	var vm = this;
       vm.test="This is a test";
-
+      console.log(vm.test);
+      
+      $http.get("https://jsonplaceholder.typicode.com/posts/1")
+    .then(function(response) {
+      vm.resultData = response.data;
+      console.log(vm.resultData);
+    });
 
 
     };
 
-    angular.module('bankingApp').controller('bbvaController', [BBVAController]);
+    angular.module('bankingApp').controller('bbvaController', ['$http',BBVAController]);
 }());
